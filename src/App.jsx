ac1dcbf4e0d55df055eee3f553f1a3651b07840c
deaoeon <deaoeon1@gmail.com>
2025-08-25 13:01:29 +0900

    0825/1:01

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import GlobalStyle from './styled/GlobalStyle';
 import Layout from './common/Layout';
+import { NotFiles, Ksports } from './pages';
 
 const App = () => {
     return (
@@ -9,6 +10,8 @@ const App = () => {
                 <GlobalStyle />
                 <Routes>
                     <Route path="/" element={<Layout />}>
+                        <Route path="ksports" element={<Ksports />} />
+
                         <Route path="*" element={<NotFiles />} />
                     </Route>
                 </Routes>
--- src/pages/notfiles/index.jsx
+++ src/pages/notfiles/index.jsx
@@ -3,7 +3,7 @@ import { NotFileStyle } from './style';
 const NotFiles = () => {
     return (
         <NotFileStyle>
-            <div className='inner'>
+            <div className="inner">
                 <h2>페이지 찾을수 없습니다.</h2>
             </div>
         </NotFileStyle>
