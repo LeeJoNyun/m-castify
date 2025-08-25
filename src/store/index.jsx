53c159cd27b821ab488d32a175ccb6bc2699cccb
SongTam-tam <115512931+LeeJoNyun@users.noreply.github.com>
2025-08-25 18:29:17 +0900

    커밋

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,9 @@
-// const store = configureStore({
-//   reducer: {},
-// });
+import { configureStore } from '@reduxjs/toolkit';
+import header from './modules/headerSlice';
+const store = configureStore({
+    reducer: {
+        header,
+    },
+});
 
-// export default store;
+export default store;
